bd22eb02a0e8c1394988d780941b6dcd95c8e677
Kaveen Kumarasinghe <ocfinancesmc@gmail.com>
2018-12-09 02:29:56 -0500

    Added libraries, added radius circle around points
    Added options to display all the different entities separately too

--- web/php/test.php
+++ web/php/test.php
@@ -2,25 +2,31 @@
 
 $showHospitals = $_GET["showHospitals"];
 $showSchools = $_GET["showSchools"];
+$showLibraries = $_GET["showLibraries"];
+
+if ((int)$_GET["schoolpriority"] + (int)$_GET["librarypriority"] + (int)$_GET["hospitalpriority"] == 0) {
+    $array = array('k_num' => (int)$_GET["k_val"], 'poi' => array('school' => 1, 'hospital' => 1, 'libraries' => 1));
+} else {
 
 
-$array = array('k_num'=>(int) $_GET["k_val"],'poi'=> array('school'=>(int) $_GET["schoolpriority"],'hospital'=>(int) $_GET["hospitalpriority"]));
+    $array = array('k_num' => (int)$_GET["k_val"], 'poi' => array('school' => (int)$_GET["schoolpriority"], 'hospital' => (int)$_GET["hospitalpriority"], 'libraries' => (int)$_GET["librarypriority"]));
+}
 $craftedmessage = json_encode($array);
-$response = send("localhost:5216",$craftedmessage);
+$response = send("localhost:5216", $craftedmessage);
 $resArr = array();
 $resArr = json_decode($response);
 //echo "<pre>"; print_r($resArr); echo "</pre>";
 $resArrEncoded = json_encode($resArr);
 echo $resArrEncoded;
 
-header("Location: https://kaveenk.me/hth/testframedisplay.php?json=".$resArrEncoded."&showHospitals=".$showHospitals."&k_val=".$_GET["k_val"]."&schoolpriority=".$_GET["schoolpriority"]."&hospitalpriority=".$_GET["hospitalpriority"]."&showSchools=".$showSchools);
+header("Location: https://kaveenk.me/hth/testframedisplay.php?json=" . $resArrEncoded . "&showHospitals=" . $showHospitals . "&k_val=" . $_GET["k_val"] . "&schoolpriority=" . $_GET["schoolpriority"] . "&hospitalpriority=" . $_GET["hospitalpriority"] . "&showSchools=" . $showSchools . "&librarypriority=" . $_GET["librarypriority"] . "&showLibraries=" . $showLibraries);
 
 
-function send($url,$message)
+function send($url, $message)
 {
     $options = array(
-        CURLOPT_RETURNTRANSFER => true,   
-        CURLOPT_HEADER => false, 
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_HEADER => false,
         CURLOPT_HTTPHEADER => array("Content-type: application/json"),
         CURLOPT_ENCODING => "",
         CURLOPT_CONNECTTIMEOUT => 120,
--- web/testframedisplay.php
+++ web/testframedisplay.php
@@ -1,9 +1,9 @@
-
 <?php
 $displaying = false;
 $coordinates = null;
 $showHospitals = $_GET["showHospitals"];
 $showSchools = $_GET["showSchools"];
+$showLibraries = $_GET["showLibraries"];
 
 if (!empty($_GET["json"])) {
     $coordinates = $_GET["json"];
@@ -50,56 +50,68 @@ if (!empty($_GET["json"])) {
     <div class="row">
         <div class="four columns" style="margin-top: 3%">
             <center><h4><b>Location Optimizer</b></h4>
-            <form method="GET" action="php/test.php">
+                <form method="GET" action="php/test.php">
 
-                <div>
-                    <label for="schoolpriority">School Priority</label>
-                    <input style="width: 100%;" value=<?php
-                    echo (int)$_GET["schoolpriority"];
+                    <div>
+                        <label for="schoolpriority">School Priority</label>
+                        <input style="width: 100%;" value=<?php
+                        echo (int)$_GET["schoolpriority"];
 
-                    ?> type="range" id="schoolpriority" name="schoolpriority"
-                           min="1" max="10">
+                        ?> type="range" id="schoolpriority" name="schoolpriority"
+                               min="0" max="10">
 
-                </div>
-                <div>
-                    <label for="hospitalpriority">Hospital Priority</label>
-                    <input style="width: 100%;" value= <?php
-                    echo (int)$_GET["hospitalpriority"];
-                    ?> type="range" id="hospitalpriority" name="hospitalpriority"
-                           min="1" max="10">
+                    </div>
+                    <div>
+                        <label for="hospitalpriority">Hospital Priority</label>
+                        <input style="width: 100%;" value= <?php
+                        echo (int)$_GET["hospitalpriority"];
+                        ?> type="range" id="hospitalpriority" name="hospitalpriority"
+                               min="0" max="10">
 
-                </div>
-                <div>
-                    <label for="k_value">K Value</label>
+                    </div>
+                    <div>
+                        <label for="librarypriority">Library Priority</label>
+                        <input style="width: 100%;" value= <?php
+                        echo (int)$_GET["librarypriority"];
+                        ?> type="range" id="librarypriority" name="librarypriority"
+                               min="0" max="10">
 
-                    <input type="number" default=1 value=<?php
-                    echo (int) $_GET["k_val"];
-                    ?> id="kval" name="k_val"
-                           min="1" max="10"></div>
-                Show Hospitals:  <input type="checkbox" name="showHospitals" id="showhospitals" value="true" <?php if ($_GET["showHospitals"] == "true") {
-                    echo "checked";
-                }  ?> >
-                Show Schools:  <input type="checkbox" name="showSchools" id="showschools" value="true" <?php if ($_GET["showSchools"] == "true") {
-                    echo "checked";
-                }  ?> >
-                <br><input type="submit" value="Submit">
+                    </div>
+                    <div>
+                        <label for="k_value">K Value</label>
+
+                        <input type="number" default=1 value=<?php
+                        echo (int)$_GET["k_val"];
+                        ?> id="kval" name="k_val"
+                               min="1" max="10"></div>
+                    Show Hospitals: <input type="checkbox" name="showHospitals" id="showhospitals"
+                                           value="true" <?php if ($_GET["showHospitals"] == "true") {
+                        echo "checked";
+                    } ?> >
+                    Show Schools: <input type="checkbox" name="showSchools" id="showschools"
+                                         value="true" <?php if ($_GET["showSchools"] == "true") {
+                        echo "checked";
+                    } ?> >
+                    Show Libraries: <input type="checkbox" name="showLibraries" id="showlibraries"
+                                           value="true" <?php if ($_GET["showLibraries"] == "true") {
+                        echo "checked";
+                    } ?> >
+                    <br><input type="submit" value="Submit">
 
 
-
-
-        </form>
+                </form>
             </center>
         </div>
         <div class="eight columns" style="margin-top:3%">
             <?php
 
             if ($displaying == true) {
-                $url = "https://kaveenk.me/hth/innerdisplay.php?json=".$coordinates."&showHospitals=".$showHospitals."&showSchools=".$showSchools;
-            echo "<iframe width='820' height='600' src='".$url."'</iframe>";
-             } else {
+                $url = "https://kaveenk.me/hth/innerdisplay.php?json=" . $coordinates . "&showHospitals=" . $showHospitals . "&showSchools=" . $showSchools . "&showLibraries=" . $showLibraries;
+                echo "<iframe width='820' height='600' src='" . $url . "'</iframe>";
+            } else {
                 echo "<iframe width='820' height='600' src='https://kaveenk.me/hth/innerdisplay.php'</iframe>";
             }
-              ?>
+            ?>
         </div>
     </div>
 </div>
